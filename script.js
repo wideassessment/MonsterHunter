93f9a13aeec507dacf5832bfdb9609a4a9973186
Andreas Hammerbeck <andreas_93@hotmail.com>
2020-02-05 16:59:49 +0100

    finalize?

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class RPG {
             {id: 0, name: "Mimic", hp: 50, ap: 8, armor:0, text: "The mimic bites you, you take ", img: 'img/mimic.svg'},
             {id: 1, name: "Beholder", hp: 100, ap: 15, armor:2, text: "The beholder uses Disintigrate, you take ", img: 'img/beholder.svg'}
         ]
-        this.initalPlayer =  {name: "Nott the Brave!", hp: 50, ap: 10, armor:0, weapon: 0, text: "You stab the monster using your dagger, it takes "}
+        this.initalPlayer =  {name: "Nott the Brave!", hp: 100, ap: 10, armor:0, weapon: 0, text: "You stab the monster using your dagger, it takes ", gold: 0}
         this.currentMonster = 0
         this.monster = {
             setHp(newHp) {
@@ -29,13 +29,21 @@ class RPG {
             setGold(newGold) {
                 gold = newGold
             },
+            setAll(player) {
+                this.hp= player.hp
+                this.ap= player.ap
+                this.armor= player.armor
+                this.gold= player.gold
+            },
             name: "Nott the Brave!", hp: 100, ap: 10, armor:10, weapon: 0, text: "You stab the monster using your dagger, it takes ", img: 'img/nottthebrave.svg', gold: 0 
         } 
         
     }
 
     initialize(){
+        this.player.setAll(this.initalPlayer)
         this.selectMonster(0)
+        
     }
     selectMonster(id)  {
         this.monster.setAll(this.monsters[id])
@@ -47,9 +55,12 @@ class RPG {
         document.getElementById("playerImg").src = this.player.img
         document.getElementById("playerName").innerHTML = this.player.name
         document.getElementById("playerHp").innerHTML = this.player.hp
+        document.getElementById("playerAp").innerHTML = "Attack Power: " + this.player.ap
+        document.getElementById("playerArmor").innerHTML = "Armor Rating: " + this.player.armor
         document.getElementById("monsterName").innerHTML = this.monster.name
         document.getElementById("monsterHp").innerHTML = this.monster.hp
-        
+        document.getElementById("monsterAp").innerHTML = "Attack Power: " + this.monster.ap
+        document.getElementById("monsterArmor").innerHTML = "Armor Rating: " + this.monster.armor
         document.getElementById("playerText").innerHTML = ""
         document.getElementById("monsterText").innerHTML = ""
     }
@@ -58,10 +69,13 @@ class RPG {
         this.selectMonster(this.currentMonster)
     }
     buySword(){
+        this.player.ap +=2
+        alert("You have upgraded your sword")
 
     }
     buyArmor() {
-
+        this.player.armor +=2
+        alert("You have upgraded your armor")
     }
     attack() {
         const playerDmg = Math.max(0,((Math.floor(Math.random() * this.player.ap+5) + this.player.ap-5) - this.monster.armor))
