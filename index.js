64205d199dd8e2eb48b71dd85d6becbc47f24002
Andreas Hammerbeck <andreas_93@hotmail.com>
2020-02-05 13:29:13 +0100

    update script

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 
 class RPG {
     constructor() {
+        this.initalPlayer =  {name: "Nott the Brave!", hp: 50, ap: 10, armor:0, weapon: 0, playerText: "You stab the monster using your dagger, it takes "}
         this.monster = {
             setHp(newHp) {
                 hp = newHp
             },
-            name: "Beholder", hp: 100, ap: 15, armor:2
+            name: "Beholder", hp: 100, ap: 15, armor:2, monsterText: "The beholder uses Disintigrate, you take "
         }
 
         this.player = {
+            setPlayer(player) {
+                this = player
+            },
             setHp(newHp) {
                 hp = newHp
             },
-            name: "Sir KnightMan!", hp: 50, ap: 10, armor:4
         } 
     }
     get playerName(){
@@ -36,18 +39,28 @@ class RPG {
         this.monster.hp = this.monster.hp - skade
     }
     initialize(){
+        this.player = this.initalPlayer
         document.getElementById("playerName").innerHTML = this.player.name
         document.getElementById("playerHp").innerHTML = this.player.hp
         document.getElementById("monsterName").innerHTML = this.monster.name
         document.getElementById("monsterHp").innerHTML = this.monster.hp
+        document.getElementById("playerText").innerHTML = ""
+        document.getElementById("monsterText").innerHTML = ""
     }
     attack() {
-        this.player.hp = this.player.hp - this.monster.ap
-        this.monster.hp = this.monster.hp - this.player.ap
+        this.player.hp = this.player.hp - (this.monster.ap - this.player.armor)
+        this.monster.hp = this.monster.hp - (this.player.ap - this.monster.armor)
+        
+        if(this.player.hp <= 0) alert("Game over!")
+        if(this.monster.hp <= 0)  {
+            alert("Congratulations! You killed the dangerous monster.")
 
+        } 
 
         document.getElementById("playerHp").innerHTML = this.player.hp
         document.getElementById("monsterHp").innerHTML = this.monster.hp
+        document.getElementById("playerText").innerHTML = this.player.playerText + (this.player.ap - this.monster.armor) + " damage."
+        document.getElementById("monsterText").innerHTML = this.monster.monsterText + (this.monster.ap - this.player.armor) + " damage."
     }
 
 }
